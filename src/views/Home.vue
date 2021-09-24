ca01652ac7139210ed370f8801fbd2dea30a04d9
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-09-24 22:17:45 +0530

    [Niraj]: Minor changes in video-bg content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     
     <video class="main-bg" autoplay muted loop>
-      <source src="@/assets/timelapse.mp4" type="video/mp4" />
+      <source src="@/assets/discovery.mp4" type="video/mp4" />
     </video>
     
   </div>
@@ -24,14 +24,10 @@ export default class Home extends Vue {}
 .home {
   overflow: hidden;
   width: 100%;
-  height: 100%;
   position: relative;
   video.main-bg {
-    min-width: 100vw;
+    min-width: 100%;
     min-height: 100vh;
-    left: 0;
-    top: 0;
-    // position: absolute;
   }
 }
 </style>
