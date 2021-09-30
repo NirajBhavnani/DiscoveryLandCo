15dec7d9cecb54a752105cd0fae0b9e7e7d1115b
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-09-30 20:55:52 +0530

    [Niraj]: Bakers-Image Section added in the about-dlc

--- src/views/AboutDlc/AboutDlc.vue
+++ src/views/AboutDlc/AboutDlc.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    <div class="about-dlc">
+  <div class="about-dlc">
+    <div class="about-dlc-title">
       <Title :bText="elements.boldText" :lText="elements.lightText" :imageBg="require('../../assets/aboutBg.jpg')"/>
     </div>
       <IntroSection :paragraphs="elements.introParas" :title="elements.title" :txtParagraphs="elements.txtParagraphs" :image="require('../../assets/IntroSection/AboutDlc.jpg')"/>
+      <div class="bakers-image-section">
+      <img
+        class="bakers-image"
+        :src="require('../../assets/AboutDlc/Bakers_Carousel.jpg')"
+        alt="BakersImg"
+      />
+    </div>
       <ExploreCarousel :carouselH1="elements.carouselH1" :carousel="elements.carousel" worldPath="AboutDlc"/>
       <TxtImg2 :header1="elements.header1" :paragraph="elements.paragraph" :imgRight="elements.imgRight" :imgLeft="elements.imgLeft"/>
       <BottomSection picture="AboutDlc.jpg"/>
