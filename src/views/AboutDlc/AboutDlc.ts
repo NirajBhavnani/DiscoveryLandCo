d0137078cae7ec27f9d61df50bffe7f600c66256
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-09-30 09:26:35 +0530

    [Niraj]: About DLC page added - Carousel

--- src/views/AboutDlc/AboutDlc.ts
+++ src/views/AboutDlc/AboutDlc.ts
@@ -1,17 +1,20 @@
 import { Options, Vue } from "vue-class-component";
 import Title from "@/components/Title/Title.vue";
+import ExploreCarousel from "@/components/ExploreCarousel/ExploreCarousel.vue";
 
 @Options({
   name: "AboutDlc",
   components: {
     Title,
+    ExploreCarousel
   },
   data() {
     return {
       elements: {} as {
         boldText: string;
         lightText: string;
-        
+        carousel: [];
+        carouselH1: string;
       }[],
     };
   },
@@ -19,7 +22,59 @@ import Title from "@/components/Title/Title.vue";
     this.elements = {
       boldText: "Creating your",
       lightText: "perfect world",
-      
+      carousel: [
+        {
+            pic: "p1.jpg",
+            title: "Focus on family",
+            location: `Our properties have always been inspired by and created for families. We build settings that are as welcoming to everyone as they are exclusive.`,
+          },
+          {
+            pic: "p2.jpg",
+            title: "Incomparable experiences",
+            location: `World-renowned golf courses, state-of-the-art fitness facilities, unique outdoor concierge programs – our communities deliver unmatched amenities and service.`,
+          },
+          {
+            pic: "p3.jpg",
+            title: "Inspired by land",
+            location: `Each property respectfully integrates the local architecture, culture, and cuisine to create an authentic and immersive experience.`,
+          },
+          {
+            pic: "p4.jpg",
+            title: "Sustainable future",
+            location: `Through a focus on sustainable efforts, we create programs that ensure we are stewards of the land and a source of local economic growth and stability.`,
+          },
+          {
+            pic: "p5.jpg",
+            title: "Luxury bespoke homes",
+            location: `Through our in-house services, Discovery Builders and Discovery Design, we offer turn-key homes for families that embody modern aesthetics while staying true to classic principles.`,
+          },
+          {
+            pic: "p1.jpg",
+            title: "Focus on family",
+            location: `Our properties have always been inspired by and created for families. We build settings that are as welcoming to everyone as they are exclusive.`,
+          },
+          {
+            pic: "p2.jpg",
+            title: "Incomparable experiences",
+            location: `World-renowned golf courses, state-of-the-art fitness facilities, unique outdoor concierge programs – our communities deliver unmatched amenities and service.`,
+          },
+          {
+            pic: "p3.jpg",
+            title: "Inspired by land",
+            location: `Each property respectfully integrates the local architecture, culture, and cuisine to create an authentic and immersive experience.`,
+          },
+          {
+            pic: "p4.jpg",
+            title: "Sustainable future",
+            location: `Through a focus on sustainable efforts, we create programs that ensure we are stewards of the land and a source of local economic growth and stability.`,
+          },
+          {
+            pic: "p5.jpg",
+            title: "Luxury bespoke homes",
+            location: `Through our in-house services, Discovery Builders and Discovery Design, we offer turn-key homes for families that embody modern aesthetics while staying true to classic principles.`,
+          },
+      ],
+      carouselH1: "The Discovery Difference",
     };
   },
 })
--- src/views/AboutDlc/AboutDlc.vue
+++ src/views/AboutDlc/AboutDlc.vue
@@ -3,6 +3,7 @@
     <div class="about-dlc">
       <Title :bText="elements.boldText" :lText="elements.lightText" :imageBg="require('../../assets/aboutBg.jpg')"/>
     </div>
+      <ExploreCarousel :carouselH1="elements.carouselH1" :carousel="elements.carousel" worldPath="AboutDlc"/>
   </div>
 </template>
 
